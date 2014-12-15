eddc3381cf5cb2b61c3cfda4593dbb9d6e92c3ff
Ivaylo Getov <contact@ivaylogetov.com>
2014-12-14 18:59:21 -0800

    ugh

--- src/ofApp.h
+++ src/ofApp.h
@@ -2,13 +2,25 @@
 
 #include "ofMain.h"
 #include "ofxOculusDK2.h"
-#include "ofxFPSCamera.h"
 
-class ofApp : public ofBaseApp{
+typedef struct{
+    ofColor color;
+    ofVec3f pos;
+    ofVec3f floatPos;
+    float radius;
+    bool bMouseOver;
+    bool bGazeOver;
+} DemoSphere;
+
+class ofApp : public ofBaseApp
+{
 public:
+    
     void setup();
     void update();
     void draw();
+    
+    void drawScene();
     
     void keyPressed(int key);
     void keyReleased(int key);
@@ -19,4 +31,20 @@ public:
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
+    
+    ofxOculusDK2		oculusRift;
+    
+    ofLight				light;
+    ofEasyCam			cam;
+    bool showOverlay;
+    bool predictive;
+    vector<DemoSphere> demos;
+    
+    ofVec3f cursor2D;
+    ofVec3f cursor3D;
+    
+    ofVec3f cursorRift;
+    ofVec3f demoRift;
+    
+    ofVec3f cursorGaze;
 };
